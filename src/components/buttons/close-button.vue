043cca17bf4da629edeab40164ea1268afebe985
Andrew Gergert <axedrew33@gmail.com>
2021-05-18 21:14:34 +0600

    Add close-button component

---
+++ src/components/buttons/close-button.vue
@@ -0,0 +1,28 @@
+<template>
+  <action-button
+    button-class="close round-icon floating right"
+    @button-click="handleClick"
+  >
+    X
+  </action-button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import ActionButton from './action-button.vue';
+
+export default Vue.extend({
+  name: 'CloseButton',
+  components: {
+    ActionButton
+  },
+  methods: {
+    handleClick() {
+      this.$emit('close');
+    }
+  }
+});
+</script>
+
+<style scoped></style>
