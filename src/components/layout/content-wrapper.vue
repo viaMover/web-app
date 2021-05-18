236c17f0b30b1c7a0338b52b65db024ec147dfa2
Andrew Gergert <axedrew33@gmail.com>
2021-05-18 21:15:19 +0600

    layout: Add new layout components

---
+++ src/components/layout/content-wrapper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="content-wrapper">
+    <left-rail v-if="hasLeftRail">
+      <slot name="left-rail"></slot>
+    </left-rail>
+
+    <page-container>
+      <close-button v-if="hasCloseButton" @close="handleClose" />
+      <slot></slot>
+    </page-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import LeftRail from './left-rail.vue';
+import PageContainer from './page-container.vue';
+import CloseButton from '@/components/buttons/close-button.vue';
+
+export default Vue.extend({
+  name: 'ContentWrapper',
+  components: {
+    PageContainer,
+    LeftRail,
+    CloseButton
+  },
+  props: {
+    hasLeftRail: {
+      type: Boolean,
+      default: false
+    },
+    hasCloseButton: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    handleClose(): void {
+      this.$emit('close');
+    }
+  }
+});
+</script>
---
+++ src/components/layout/left-rail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="left rail">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'LeftRail'
+});
+</script>
