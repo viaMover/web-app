60e80acb419a09f28e8df5b7aa9cffd905c8949b
divold-vb <div98@mail.ru>
2021-08-02 18:21:50 +0600

    Treasury: middle commit
    refactoring component's

---
+++ src/components/treasury/treasury-monthly-statment/index.ts
@@ -0,0 +1,1 @@
+export { default as TreasuryMonthlyStatement } from './treasury-monthly-statement.vue';
--- src/views/treasury/treasury-monthly-statistics.vue
+++ src/views/treasury/treasury-monthly-statistics.vue
@@ -1,42 +1,27 @@
 <template>
-  <secondary-page :title="$t('treasury.lblSmartTreasury')">
-    <h2>{{ pageTitle }}</h2>
-    <treasury-monthly-chart-wrapper
-      v-if="isFeatureEnabled('isTreasuryMonthlyChartEnabled')"
-    />
+  <secondary-page hide-title>
+    <!--    <treasury-monthly-chart-wrapper-->
+    <!--      v-if="isFeatureEnabled('isTreasuryMonthlyChartEnabled')"-->
+    <!--    />-->
     <treasury-monthly-statement />
   </secondary-page>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import dayjs from 'dayjs';
 
 import { isFeatureEnabled } from '@/settings';
 
 import { SecondaryPage } from '@/components/layout';
-import {
-  TreasuryMonthlyChartWrapper,
-  TreasuryMonthlyStatement
-} from '@/components/treasury';
+//import { TreasuryMonthlyChartWrapper } from '@/components/treasury';
+import { TreasuryMonthlyStatement } from '@/components/treasury/treasury-monthly-statment';
 
 export default Vue.extend({
   name: 'TreasuryMonthlyStatistics',
   components: {
     SecondaryPage,
-    TreasuryMonthlyChartWrapper,
+    //TreasuryMonthlyChartWrapper,
     TreasuryMonthlyStatement
-  },
-  computed: {
-    pageTitle(): string {
-      try {
-        const tsFrom = Number(this.$route.query.tsFrom);
-
-        return dayjs.unix(tsFrom).format('MMMM YYYY');
-      } catch {
-        return this.$t('swaps.statement.lblMonthStatisticFallback') as string;
-      }
-    }
   },
   methods: {
     isFeatureEnabled
