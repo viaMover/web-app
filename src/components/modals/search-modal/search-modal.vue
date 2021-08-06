5aa46ed3126fadc642994c359573214614088e62
Andrew Gergert <axedrew33@gmail.com>
2021-08-06 17:47:43 +0600

    Search Modal: Revert scrolling logic

--- src/components/modals/search-modal/search-modal.vue
+++ src/components/modals/search-modal/search-modal.vue
@@ -118,7 +118,7 @@ export default Vue.extend({
             return;
           }
 
-          this.$refs.resultsTop.scrollIntoView();
+          this.$refs.resultsTop.scroll({ top: 0 });
         });
       }, this.debounceTimeout);
     },
