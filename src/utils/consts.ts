8f0d4b6ad31b5d84cbe8c81988acb9ab4e1cecf9
Pavel <trukach000@gmail.com>
2021-08-19 21:31:29 +0300

    approve fix

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -2,6 +2,6 @@ import { BigNumber } from 'bignumber.js';
 
 export const MAXUINT256 = new BigNumber(
   '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
-);
+).toFixed();
 
 export const MAX_ASSET_NAME = 30;
