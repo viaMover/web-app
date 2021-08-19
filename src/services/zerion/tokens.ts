5289677e3e3149036bca360aedc5786638bcd471
Pavel <trukach000@gmail.com>
2021-08-19 19:48:58 +0300

    removed assets with zeros decimals

--- src/services/zerion/tokens.ts
+++ src/services/zerion/tokens.ts
@@ -10,34 +10,36 @@ export const mapZerionTokens = (
   data: ZerionAssetsReceived,
   network: Network
 ): Array<TokenWithBalance> => {
-  const tokens = Object.entries(data.payload.assets).map(([hash, t]) => {
-    let assetName = '';
-    let assetSymbol = '';
-    if (sameAddress(t.asset.asset_code, getMoveAssetData(network).address)) {
-      assetName = 'Mover';
-      assetSymbol = 'MOVE';
-    } else {
-      assetName = t.asset.name;
-      if (assetName.length > MAX_ASSET_NAME) {
-        assetName = assetName.substr(0, MAX_ASSET_NAME);
+  const tokens = Object.entries(data.payload.assets)
+    .map(([hash, t]) => {
+      let assetName = '';
+      let assetSymbol = '';
+      if (sameAddress(t.asset.asset_code, getMoveAssetData(network).address)) {
+        assetName = 'Mover';
+        assetSymbol = 'MOVE';
+      } else {
+        assetName = t.asset.name;
+        if (assetName.length > MAX_ASSET_NAME) {
+          assetName = assetName.substr(0, MAX_ASSET_NAME);
+        }
+        assetSymbol = t.asset.symbol;
       }
-      assetSymbol = t.asset.symbol;
-    }
 
-    if (assetSymbol === 'mobo') {
-      assetSymbol = 'MOBO';
-    }
+      if (assetSymbol === 'mobo') {
+        assetSymbol = 'MOBO';
+      }
 
-    return {
-      address: t.asset.asset_code,
-      balance: fromWei(t.quantity, t.asset.decimals),
-      decimals: t.asset.decimals,
-      logo: t.asset.icon_url ?? '',
-      name: assetName,
-      symbol: assetSymbol,
-      priceUSD: t.asset.price?.value ? String(t.asset.price?.value) : '0'
-    } as TokenWithBalance;
-  });
+      return {
+        address: t.asset.asset_code,
+        balance: fromWei(t.quantity, t.asset.decimals),
+        decimals: t.asset.decimals,
+        logo: t.asset.icon_url ?? '',
+        name: assetName,
+        symbol: assetSymbol,
+        priceUSD: t.asset.price?.value ? String(t.asset.price?.value) : '0'
+      } as TokenWithBalance;
+    })
+    .filter((t) => t.decimals > 0);
 
   return tokens;
 };
