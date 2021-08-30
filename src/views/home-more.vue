7a8f65e89b04bd9d5f5c5e1cdc5c71849fbf571d
Andrew Gergert <axedrew33@gmail.com>
2021-08-30 21:36:05 +0600

    Views: Add 'home-more' page
    Also changed missing name to the valid one

---
+++ src/views/home-more.vue
@@ -0,0 +1,33 @@
+<template>
+  <content-wrapper has-left-rail wrapper-class="general-desktop">
+    <governance-section v-if="isFeatureEnabled('isGovernanceEnabled')" />
+    <nibble-shop-section v-if="isFeatureEnabled('isNibbleShopEnabled')" />
+    <nft-drops-section v-if="isFeatureEnabled('isNftDropsEnabled')" />
+  </content-wrapper>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import { isFeatureEnabled } from '@/settings';
+
+import { ContentWrapper } from '@/components/layout';
+import {
+  GovernanceSection,
+  NibbleShopSection,
+  NftDropsSection
+} from '@/components/sections';
+
+export default Vue.extend({
+  name: 'HomeMore',
+  components: {
+    ContentWrapper,
+    GovernanceSection,
+    NibbleShopSection,
+    NftDropsSection
+  },
+  methods: {
+    isFeatureEnabled
+  }
+});
+</script>
