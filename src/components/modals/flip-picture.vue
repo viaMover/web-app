e30446f54ef072568d417fd4adcf75d978d27c56
Andrew Gergert <axedrew33@gmail.com>
2021-09-08 18:03:15 +0600

    Components: Add picture components to be reused

---
+++ src/components/modals/flip-picture.vue
@@ -0,0 +1,34 @@
+<template>
+  <custom-picture
+    :alt="$t('icon.txtFlipAssetsIconAlt')"
+    :sources="pictureSources"
+    :src="pictureSrc"
+    :webp-sources="pictureWebpSources"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import { CustomPicture, PictureSourceDescriptor } from '@/components/html5';
+
+export default Vue.extend({
+  name: 'FlipPicture',
+  components: {
+    CustomPicture
+  },
+  data() {
+    return {
+      pictureSrc: require('@/assets/images/Flip.png'),
+      pictureSources: [
+        { src: require('@/assets/images/Flip.png') },
+        { src: require('@/assets/images/Flip@2x.png'), variant: '2x' }
+      ] as Array<PictureSourceDescriptor>,
+      pictureWebpSources: [
+        { src: require('@/assets/images/Flip.webp') },
+        { src: require('@/assets/images/Flip@2x.webp'), variant: '2x' }
+      ] as Array<PictureSourceDescriptor>
+    };
+  }
+});
+</script>
