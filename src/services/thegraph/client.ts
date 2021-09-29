fdc48c2557501989dae56e81f922be7f26048d41
Andrew Gergert <axedrew33@gmail.com>
2021-09-29 23:29:34 +0600

    Chore: Remove unused import

--- src/services/thegraph/client.ts
+++ src/services/thegraph/client.ts
@@ -1,4 +1,3 @@
-import { SUSHISWAP_PRICES_QUERY } from './queries';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
