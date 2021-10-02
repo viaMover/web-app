4b1428e26f1c66eabf42e2367a6ec2ea648b3907
Andrew Gergert <axedrew33@gmail.com>
2021-10-02 21:42:41 +0600

    Perf/Debug: Enable component tracking of Sentry

--- src/main.ts
+++ src/main.ts
@@ -49,7 +49,8 @@ if (!isDevelop()) {
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
     logErrors: true,
-    environment: process.env.NODE_ENV
+    environment: process.env.NODE_ENV,
+    trackComponents: true
   });
 }
 
