7281aac98c91af362c60e0f78970c347ba51cd89
Andrew Gergert <axedrew33@gmail.com>
2021-10-06 18:55:14 +0600

    Services: Add governance onchain service

---
+++ src/services/chain/governance/governance.ts
@@ -0,0 +1,44 @@
+import Web3 from 'web3';
+import { Network } from '@/utils/networkTypes';
+import { TransactionsParams } from '@/wallet/types';
+import {
+  MOVE_ADDRESS,
+  ERC20_ABI,
+  SUSHISWAP_MOVE_WETH_POOL_ADDRESS,
+  SUSHI_UNI_PAIR_V2_ABI
+} from '@/wallet/references/data';
+import { AbiItem } from 'web3-utils';
+import { add, fromWei, multiply } from '@/utils/bigmath';
+
+export const getCommunityVotingPower = async (
+  accountAddress: string,
+  network: Network,
+  web3: Web3
+): Promise<string> => {
+  const transactionParams: TransactionsParams = {
+    from: accountAddress
+  };
+
+  const moveTokenAddress = MOVE_ADDRESS(network);
+  const moveToken = new web3.eth.Contract(
+    ERC20_ABI as AbiItem[],
+    moveTokenAddress
+  );
+  const moveTotalSupplyInWei = await moveToken.methods
+    .totalSupply()
+    .call(transactionParams);
+
+  const sushiPoolAddress = SUSHISWAP_MOVE_WETH_POOL_ADDRESS(network);
+  const sushiPool = new web3.eth.Contract(
+    SUSHI_UNI_PAIR_V2_ABI as AbiItem[],
+    sushiPoolAddress
+  );
+  const sushiReservesInWei = await sushiPool.methods
+    .getReserves()
+    .call(transactionParams);
+
+  const moveTotalSupply = fromWei(moveTotalSupplyInWei, 18);
+  const sushiReserveMovePart = fromWei(sushiReservesInWei._reserve0, 18);
+
+  return add(moveTotalSupply, multiply(sushiReserveMovePart, '1.5'));
+};
