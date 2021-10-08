3fd2e4f40ef68f06d64d554c6644645576357865
Divold <div98@mail.ru>
2021-10-08 17:41:49 +0600

    Styling: fixes

--- src/components/savings/savings-deposit-form.vue
+++ src/components/savings/savings-deposit-form.vue
@@ -30,6 +30,7 @@
         </div>
         <button
           class="button-active button-arrow"
+          :style="selectorStyle"
           type="button"
           @click.stop.prevent="handleOpenSelectModal"
         >
@@ -72,6 +73,7 @@
             {{ $t('savings.deposit.lblSwappingFor') }}
             <custom-picture
               alt="USDC"
+              class="token"
               :sources="usdcPicture.sources"
               :src="usdcPicture.src"
             />
@@ -101,6 +103,7 @@ import { mapActions, mapGetters, mapState } from 'vuex';
 
 import * as Sentry from '@sentry/vue';
 import BigNumber from 'bignumber.js';
+import { Properties as CssProperties } from 'csstype';
 
 import {
   getTransferData,
@@ -123,8 +126,10 @@ import {
   toWei
 } from '@/utils/bigmath';
 import { formatToDecimals, formatToNative } from '@/utils/format';
-import { estimateDepositCompound } from '@/wallet/actions/savings/deposit/depositEstimate';
-import { CompoudEstimateResponse } from '@/wallet/actions/savings/deposit/depositEstimate';
+import {
+  CompoudEstimateResponse,
+  estimateDepositCompound
+} from '@/wallet/actions/savings/deposit/depositEstimate';
 import {
   calcTransactionFastNativePrice,
   isSubsidizedAllowed
@@ -313,6 +318,19 @@ export default Vue.extend({
       }
 
       return `${formatToNative(boughtUSDC)} USDC`;
+    },
+    selectorStyle(): CssProperties {
+      if (this.asset === undefined || this.asset.color === undefined) {
+        return {
+          backgroundColor: '#687EE3',
+          boxShadow: '0 0 8px #687EE3'
+        };
+      }
+
+      return {
+        backgroundColor: this.asset.color,
+        boxShadow: `0 0 8px ${this.asset.color}`
+      };
     }
   },
   watch: {
