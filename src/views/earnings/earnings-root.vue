0001bdb40e26b87ea892c339e23d5e87aaa8998b
Andrew Gergert <axedrew33@gmail.com>
2021-10-15 22:05:52 +0600

    Earnings: Add new views (bolderplate)

---
+++ src/views/earnings/earnings-root.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapActions } from 'vuex';
+
+export default Vue.extend({
+  name: 'EarningsRoot',
+  async mounted() {
+    await this.loadMinimalInfo();
+  },
+  methods: {
+    ...mapActions('earnings', {
+      loadMinimalInfo: 'loadMinimalInfo'
+    })
+  }
+});
+</script>
---
+++ src/views/earnings/olympus/earnings-olympus-root.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapActions } from 'vuex';
+
+export default Vue.extend({
+  name: 'EarningsOlympusRoot',
+  async mounted() {
+    await this.loadInfo();
+  },
+  methods: {
+    ...mapActions('earnings/olympus', {
+      loadInfo: 'loadInfo'
+    })
+  }
+});
+</script>
