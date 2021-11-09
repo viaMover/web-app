541c860cf977046c76637512223a5eaaed748a85
Andrew Gergert <axedrew33@gmail.com>
2021-11-09 19:53:18 +0600

    Feature: Change mock debit card initial state
    Work in progress, to be changed to use actual API

--- src/services/mover/debit-card/service.ts
+++ src/services/mover/debit-card/service.ts
@@ -24,7 +24,7 @@ export const getCardInfo = async (
   return {
     isError: false,
     result: {
-      state: 'order_now',
+      state: 'active',
       eventHistory: [
         {
           timestamp: 1630215570,
