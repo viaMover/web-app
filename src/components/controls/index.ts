fcb6b4c37b32a82f40b89bd78b5cad72617dcc36
Andrew Gergert <axedrew33@gmail.com>
2022-01-25 18:20:25 +0600

    Refactor: Remove unused components / private yet exposed imports

--- src/components/controls/index.ts
+++ src/components/controls/index.ts
@@ -1,14 +1,8 @@
 export { default as AssetField } from './asset-field.vue';
-export { default as TextInputField } from './text-input-field.vue';
 export { default as GasSelector } from './gas-selector.vue';
 export { default as SlippageSelector } from './slippage-selector.vue';
 export { default as PlusIcon } from './plus-icon.vue';
-export { default as PriceInputField } from './price-input-field.vue';
 export { default as ArrowDownIcon } from './arrow-down-icon.vue';
-export { default as ArrowLeftIcon } from './arrow-left-icon.vue';
-export { default as NavBarItem } from './nav-bar-item.vue';
-export { default as NavBar } from './nav-bar.vue';
-export { default as EmojiCard } from './emoji-card.vue';
 export { default as Card } from './card.vue';
 export { default as DynamicInput } from './dynamic-input.vue';
 export { default as CustomSwitch } from './custom-switch.vue';
