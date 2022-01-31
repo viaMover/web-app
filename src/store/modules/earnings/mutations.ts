3fdab0caea3412b95fb1a27f300fb953da598c65
Divold <div98@mail.ru>
2022-01-31 18:24:43 +0600

    Store-split: middle commit
    modify store types
    apply new types

--- src/store/modules/earnings/mutations.ts
+++ src/store/modules/earnings/mutations.ts
@@ -3,14 +3,10 @@ import { MutationFuncs } from '@/store/types';
 import { EarningsStoreState } from './types';
 
 type Mutations = {
-  doNothing: void;
+  //
 };
 
-const mutations: MutationFuncs<Mutations, EarningsStoreState> = {
-  doNothing(): void {
-    //;
-  }
-};
+const mutations: MutationFuncs<Mutations, EarningsStoreState> = {};
 
 export type MutationType = typeof mutations;
 export default mutations;
