da11b09aeeb6af29ee984687511901ac169ba7cd
Andrew Gergert <axedrew33@gmail.com>
2022-02-03 18:39:48 +0600

    Chore: Remove unused governance components

--- src/components/governance/governance-overview-section/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as GovernanceOverviewSection } from './governance-overview-section.vue';
-export { default as GovernanceOverviewSectionSkeleton } from './governance-overview-section-skeleton.vue';
-export { default as GovernanceOverviewSectionItem } from './governance-overview-section-item.vue';
-export { default as GovernanceOverviewSectionItemSkeleton } from './governance-overview-section-item-skeleton.vue';
