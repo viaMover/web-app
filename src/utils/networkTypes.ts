95c786cb145b44676b19b91b22204e87de06369b
Pavel <trukach000@gmail.com>
2022-02-16 18:20:12 +0200

    fix

--- src/utils/networkTypes.ts
+++ src/utils/networkTypes.ts
@@ -1,3 +1,5 @@
+import { SmallTokenInfoWithIcon } from '@/wallet/types';
+
 export enum Network {
   binance = 'binance',
   binanceTest = 'binanceTest',
@@ -16,6 +18,9 @@ export type NetworkInfo = {
   chainId: number;
   explorer: string;
   subsidizedUrl?: string;
+  baseAsset: SmallTokenInfoWithIcon & {
+    name: string;
+  };
 };
 
 export const networks = new Array<NetworkInfo>(
@@ -23,61 +28,141 @@ export const networks = new Array<NetworkInfo>(
     chainId: 1,
     network: Network.mainnet,
     explorer: 'https://etherscan.io',
-    subsidizedUrl: 'https://api.viamover.com/api/v1'
+    subsidizedUrl: 'https://api.viamover.com/api/v1',
+    baseAsset: {
+      address: 'eth',
+      decimals: 18,
+      symbol: 'ETH',
+      name: 'Ethereum',
+      iconURL:
+        'https://github.com/trustwallet/assets/raw/master/blockchains/ethereum/info/logo.png'
+    }
   },
   {
     chainId: 3,
     network: Network.ropsten,
     explorer: 'https://ropsten.etherscan.io',
-    subsidizedUrl: undefined
+    subsidizedUrl: undefined,
+    baseAsset: {
+      address: 'eth',
+      decimals: 18,
+      symbol: 'ETH',
+      name: 'Ethereum',
+      iconURL:
+        'https://github.com/trustwallet/assets/raw/master/blockchains/ethereum/info/logo.png'
+    }
   },
   {
     chainId: 4,
     network: Network.rinkeby,
     explorer: 'https://rinkeby.etherscan.io',
-    subsidizedUrl: undefined
+    subsidizedUrl: undefined,
+    baseAsset: {
+      address: 'eth',
+      decimals: 18,
+      symbol: 'ETH',
+      name: 'Ethereum',
+      iconURL:
+        'https://github.com/trustwallet/assets/raw/master/blockchains/ethereum/info/logo.png'
+    }
   },
   {
     chainId: 42,
     network: Network.kovan,
     explorer: 'https://kovan.etherscan.io',
-    subsidizedUrl: undefined
+    subsidizedUrl: undefined,
+    baseAsset: {
+      address: 'eth',
+      decimals: 18,
+      symbol: 'ETH',
+      name: 'Ethereum',
+      iconURL:
+        'https://github.com/trustwallet/assets/raw/master/blockchains/ethereum/info/logo.png'
+    }
   },
   {
     chainId: 137,
     network: Network.matic,
     explorer: 'explorer-mainnet.maticvigil.com',
-    subsidizedUrl: undefined
+    subsidizedUrl: undefined,
+    baseAsset: {
+      address: 'eth',
+      decimals: 18,
+      symbol: 'ETH',
+      name: 'Ethereum',
+      iconURL:
+        'https://github.com/trustwallet/assets/raw/master/blockchains/ethereum/info/logo.png'
+    }
   },
   {
     chainId: 56,
     network: Network.binance,
     explorer: 'https://bscscan.com',
-    subsidizedUrl: undefined
+    subsidizedUrl: undefined,
+    baseAsset: {
+      address: 'eth',
+      decimals: 18,
+      symbol: 'ETH',
+      name: 'Ethereum',
+      iconURL:
+        'https://github.com/trustwallet/assets/raw/master/blockchains/ethereum/info/logo.png'
+    }
   },
   {
     chainId: 97,
     network: Network.binanceTest,
     explorer: 'https://testnet.bscscan.com',
-    subsidizedUrl: undefined
+    subsidizedUrl: undefined,
+    baseAsset: {
+      address: 'eth',
+      decimals: 18,
+      symbol: 'ETH',
+      name: 'Ethereum',
+      iconURL:
+        'https://github.com/trustwallet/assets/raw/master/blockchains/ethereum/info/logo.png'
+    }
   },
   {
     chainId: 43114,
     network: Network.avalanche,
     explorer: 'https://snowtrace.io/',
-    subsidizedUrl: undefined
+    subsidizedUrl: undefined,
+    baseAsset: {
+      address: 'avax',
+      decimals: 9,
+      symbol: 'AVAX',
+      name: 'Avalanche X-Chain',
+      iconURL:
+        'https://github.com/trustwallet/assets/raw/master/blockchains/avalanchex/info/logo.png'
+    }
   },
   {
     chainId: 42161,
     network: Network.arbitrum,
     explorer: 'https://arbiscan.io/',
-    subsidizedUrl: undefined
+    subsidizedUrl: undefined,
+    baseAsset: {
+      address: 'areth',
+      decimals: 18,
+      symbol: 'ARETH',
+      name: 'Arbitrum',
+      iconURL:
+        'https://github.com/trustwallet/assets/raw/master/blockchains/arbitrum/info/logo.png'
+    }
   },
   {
     chainId: 250,
     network: Network.fantom,
     explorer: 'https://ftmscan.com',
-    subsidizedUrl: undefined
+    subsidizedUrl: undefined,
+    baseAsset: {
+      address: 'ftm',
+      decimals: 18,
+      symbol: 'FTM',
+      name: 'Fantom',
+      iconURL:
+        'https://github.com/trustwallet/assets/raw/master/blockchains/fantom/info/logo.png'
+    }
   }
 );
 
