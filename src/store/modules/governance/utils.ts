bbef876f255b9458b15e02804f95fb24cdf9b817
Divold <div98@mail.ru>
2022-02-18 10:15:56 +0600

    Governance-store: fixes after code-review
    remove legacy util

--- src/store/modules/governance/utils.ts
+++ src/store/modules/governance/utils.ts
@@ -1,21 +1,3 @@
-import dayjs from 'dayjs';
-
-export const isValidCacheItem = (
-  cacheMap: Record<string, { updatedAt: number }>,
-  id: string,
-  expirationPeriod: number
-): boolean => {
-  if (cacheMap[id] === undefined) {
-    return false;
-  }
-
-  const expiresAt = dayjs
-    .unix(cacheMap[id].updatedAt)
-    .add(expirationPeriod, 'second');
-
-  return expiresAt.isAfter(dayjs());
-};
-
 export const isProviderRpcError = (
   error: unknown
 ): error is ProviderRpcError => {
