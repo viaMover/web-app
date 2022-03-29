e043208787b9cb1f6c677266a7c3c9302e8c4748
Andrew Gergert <axedrew33@gmail.com>
2022-03-29 19:40:25 +0600

    Feature: Register sub components

---
+++ src/components/staking-ubt/index.ts
@@ -0,0 +1,3 @@
+export { default as StakingUbtEmpty } from './staking-ubt-empty.vue';
+export { default as StakingUbtManage } from './staking-ubt-manage.vue';
+export { default as StakingUbtMonthlyStatement } from './staking-ubt-monthly-statement.vue';
