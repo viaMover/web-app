a797ea62aebaef60782defb62829e9a3113163ff
supa <buba-zello@protonmail.com>
2022-04-07 11:59:20 +0300

    removed redundant comment

--- src/settings/globals.ts
+++ src/settings/globals.ts
@@ -47,7 +47,6 @@ export const isDevelop = (): boolean => {
 };
 
 // enables console output in browser developer tools
-// use .env files to change it
 export const isConsoleEnabled = (): boolean => {
   return process.env.VUE_APP_CONSOLE_LOGS === 'true';
 };
