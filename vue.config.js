2799008d2ba112ceae71163c4e52b1d8802d6afd
Andrew Gergert <axedrew33@gmail.com>
2022-06-09 17:14:02 +0600

    Locale: Update vue-i18n setup, add i18n:report task

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  transpileDependencies: ['web3modal-vue']
+  transpileDependencies: ['web3modal-vue'],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'i18n/messages',
+      enableInSFC: false,
+      enableBridge: false
+    }
+  }
 };
