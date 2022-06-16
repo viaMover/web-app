8da902ff14c5721991971de77fca8c4f7be3a3d2
buba-developer <buba-zello-three@protonmail.com>
2022-06-16 17:25:30 +0600

    added yearn simple vaults

--- src/wallet/references/yearnVaultsData.ts
+++ src/wallet/references/yearnVaultsData.ts
@@ -37,6 +37,110 @@ export const addresses = {
         address: '0x6b175474e89094c44da98b954eedeac495271d0f',
         decimals: 18
       }
+    },
+    {
+      name: 'YV_WETH',
+      vaultToken: {
+        symbol: 'yvWETH',
+        address: '0xa258C4606Ca8206D8aA700cE2143D7db854D168c',
+        decimals: 18
+      },
+      commonToken: {
+        symbol: 'WETH',
+        address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+        decimals: 18
+      }
+    },
+    {
+      name: 'YV_WBTC',
+      vaultToken: {
+        symbol: 'yvWBTC',
+        address: '0xA696a63cc78DfFa1a63E9E50587C197387FF6C7E',
+        decimals: 8
+      },
+      commonToken: {
+        symbol: 'WBTC',
+        address: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+        decimals: 8
+      }
+    },
+    {
+      name: 'YV_SUSHI',
+      vaultToken: {
+        symbol: 'yvSUSHI',
+        address: '0x6d765CbE5bC922694afE112C140b8878b9FB0390',
+        decimals: 18
+      },
+      commonToken: {
+        symbol: 'SUSHI',
+        address: '0x6B3595068778DD592e39A122f4f5a5cF09C90fE2',
+        decimals: 18
+      }
+    },
+    {
+      name: 'YV_YFI',
+      vaultToken: {
+        symbol: 'yvYFI',
+        address: '0xdb25cA703181E7484a155DD612b06f57E12Be5F0',
+        decimals: 18
+      },
+      commonToken: {
+        symbol: 'YFI',
+        address: '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
+        decimals: 18
+      }
+    },
+    {
+      name: 'YV_LUSD',
+      vaultToken: {
+        symbol: 'yvLUSD',
+        address: '0x378cb52b00F9D0921cb46dFc099CFf73b42419dC',
+        decimals: 18
+      },
+      commonToken: {
+        symbol: 'LUSD',
+        address: '0x5f98805A4E8be255a32880FDeC7F6728C6568bA0',
+        decimals: 18
+      }
+    },
+    {
+      name: 'YV_SNX',
+      vaultToken: {
+        symbol: 'yvSNX',
+        address: '0xF29AE508698bDeF169B89834F76704C3B205aedf',
+        decimals: 18
+      },
+      commonToken: {
+        symbol: 'SNX',
+        address: '0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F',
+        decimals: 18
+      }
+    },
+    {
+      name: 'YV_AAVE',
+      vaultToken: {
+        symbol: 'yvAAVE',
+        address: '0xd9788f3931Ede4D5018184E198699dC6d66C1915',
+        decimals: 18
+      },
+      commonToken: {
+        symbol: 'AAVE',
+        address: '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9',
+        decimals: 18
+      }
+    },
+    {
+      name: 'YV_UNI',
+      vaultToken: {
+        symbol: 'yvUNI',
+        address: '0xFBEB78a723b8087fD2ea7Ef1afEc93d35E8Bed42',
+        decimals: 18
+      },
+      commonToken: {
+        symbol: 'UNI',
+        address: '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
+        decimals: 18
+      }
     }
   ]
 } as vaultAddressMap;
